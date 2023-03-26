b0800ea378015646a4f74736f3becfbb17608897
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2023-03-26 19:48:26 +0300

    Remove storybook build

--- storybook-static/assets/es.map.constructor-d0f2b33b.js
+++
@@ -1,2 +0,0 @@
-var r=Object.defineProperty;var t=(n,o)=>r(n,"name",{value:o,configurable:!0});import{ao as e,ap as a}from"./iframe-03c40f27.js";var c=e,i=a;c("Map",function(n){return t(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},i);
-//# sourceMappingURL=es.map.constructor-d0f2b33b.js.map
