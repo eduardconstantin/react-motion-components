9b6a467d07d1473a3a0e37d881d35083d380fca9
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2023-09-11 18:36:24 +0300

    Add random decimal helper

--- src/components/helpers.ts
+++ src/components/helpers.ts
@@ -1,1 +1,2 @@
-export const RANDOM = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1) + min);
+export const RANDOM_INT = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1) + min);
+export const RANDOM_DECIMAL = (min: number, max: number): number => Math.random() * (max - min) + min;
