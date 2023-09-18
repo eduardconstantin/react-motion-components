3683f3b7f48cf720f0d858c50e1f0984f5083d1e
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2023-09-18 20:08:26 +0300

    Add theme and brand

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,26 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
+    "@storybook/addon-mdx-gfm",
   ],
 
-  "framework": {
+  framework: {
     name: "@storybook/react-vite",
-    options: {}
+    options: {},
   },
 
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true,
   },
 
   docs: {
-    autodocs: true
-  }
-}
+    autodocs: true,
+  },
+};
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import mainTheme from "./mainTheme";
+
+addons.setConfig({
+  theme: mainTheme,
+});
