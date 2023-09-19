b05b1164108c918031a1d33e3b002cde1ef0c588
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2023-09-19 19:44:49 +0300

    Add types to menu bar

---
+++ src/components/navigation-menu/components/MenuBar/MenuBar.d.tsx
@@ -0,0 +1,12 @@
+import { MenuElement } from "../../Menu.d";
+
+export type MenuBarProps = {
+  menuElements: MenuElement[];
+  menuSize: number | null;
+  isOpen: boolean;
+  angle: number;
+};
+
+export type MenuClasses = {
+  [key: number]: string;
+};
