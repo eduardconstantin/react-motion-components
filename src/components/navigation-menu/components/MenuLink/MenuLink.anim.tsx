7cd3e7fcafe97ecc99fa6c608faf73f81c746c01
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2023-09-20 20:07:22 +0300

    Fix anim warning

--- src/components/navigation-menu/components/MenuLink/MenuLink.anim.tsx
+++ src/components/navigation-menu/components/MenuLink/MenuLink.anim.tsx
@@ -1,45 +1,51 @@
 export const iconAnim = {
-	hover: {
-		color: '#157C95',
-		fill: '#157C95',
-		y: 0,
-		filter: 'drop-shadow(0px 2px 10px rgba(117, 212, 232,1))',
-		transition: {
-			type: 'spring',
-			stiffness: 150,
-		},
-	},
-	initial: {
-		color: '#5D606F',
-		fill: '#5D606F',
-		y: 8,
-		filter: 'drop-shadow(0px 0px 0px rgba(117, 212, 232,0))',
-		transition: {
-			type: 'spring',
-			stiffness: 130,
-		},
-	},
+  hover: {
+    color: "#157C95",
+    fill: "#157C95",
+    y: 0,
+    filter: "drop-shadow(0px 2px 10px rgba(117, 212, 232, 1))",
+    transition: {
+      type: "spring",
+      stiffness: 150,
+      filter: {
+        type: "linear",
+      },
+    },
+  },
+  initial: {
+    color: "#5D606F",
+    fill: "#5D606F",
+    y: 8,
+    filter: "drop-shadow(0px 0px 0px rgba(117, 212, 232, 0))",
+    transition: {
+      type: "spring",
+      stiffness: 130,
+      filter: {
+        type: "linear",
+      },
+    },
+  },
 };
 
 export const labelAnim = {
-	hover: {
-		color: '#157C95',
-		fill: '#157C95',
-		opacity: 1,
-		y: 0,
-		transition: {
-			type: 'spring',
-			stiffness: 150,
-		},
-	},
-	initial: {
-		color: '#5D606F',
-		fill: '#5D606F',
-		opacity: 0,
-		y: -8,
-		transition: {
-			type: 'spring',
-			stiffness: 130,
-		},
-	},
+  hover: {
+    color: "#157C95",
+    fill: "#157C95",
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 150,
+    },
+  },
+  initial: {
+    color: "#5D606F",
+    fill: "#5D606F",
+    opacity: 0,
+    y: -8,
+    transition: {
+      type: "spring",
+      stiffness: 130,
+    },
+  },
 };
