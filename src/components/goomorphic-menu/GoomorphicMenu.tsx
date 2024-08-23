eef734d7eb6945f64c9e1f478bcac8133728deae
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-08-23 20:35:02 +0300

    Implement scroll on click

--- src/components/goomorphic-menu/GoomorphicMenu.tsx
+++ src/components/goomorphic-menu/GoomorphicMenu.tsx
@@ -1,10 +1,21 @@
 import { FC } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, animate } from "framer-motion";
 import { Props, Section } from "./GoomorphicMenu.d";
 import styles from "./GoomorphicMenu.module.css";
 import * as anim from "./GoomorphicMenu.anim";
 
 const GoomorphicMenu: FC<Props> = ({ section, sections }) => {
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id);
+    if (section) {
+      animate(window.scrollY, section.offsetTop, {
+        duration: 0.7,
+        onUpdate: (latest) => window.scrollTo(0, latest),
+        ease: "easeInOut",
+      });
+    }
+  };
+
   return (
     <AnimatePresence mode="wait">
       <div className={styles.header}>
@@ -13,9 +24,16 @@ const GoomorphicMenu: FC<Props> = ({ section, sections }) => {
           initial="home"
           animate={section.toLowerCase()}
           variants={anim.mainBar}
-          transition={{ type: "spring", duration: 1 }}
+          transition={{ type: "spring", duration: 0.7 }}
         >
-          <motion.div className={styles.dots} variants={anim.leftDot} transition={{ type: "spring", duration: 1 }}>
+          <motion.div
+            className={styles.dots}
+            variants={anim.leftDot}
+            transition={{ type: "spring", duration: 1 }}
+            onClick={() => {
+              scrollToSection("Home");
+            }}
+          >
             <p>Ed</p>
             <motion.span variants={anim.span} transition={{ type: "linear", duration: 0.05 }}>
               Edd & Eddie
@@ -27,15 +45,26 @@ const GoomorphicMenu: FC<Props> = ({ section, sections }) => {
           >
             {sections.slice(1, -1).map((sec: Section) => (
               <motion.li
+                id={sec.key}
                 initial={{ opacity: 0.5 }}
                 animate={sec.id === section ? { opacity: 1 } : { opacity: 0.5 }}
                 key={sec.key}
+                onClick={() => {
+                  scrollToSection(sec.id);
+                }}
               >
                 {sec.id} {sec.id === section && <motion.span layoutId="indicator"></motion.span>}
               </motion.li>
             ))}
           </motion.ul>
-          <motion.div className={styles.dots} variants={anim.rightDot} transition={{ type: "spring", duration: 1 }}>
+          <motion.div
+            className={styles.dots}
+            variants={anim.rightDot}
+            transition={{ type: "spring", duration: 1 }}
+            onClick={() => {
+              scrollToSection("Contact");
+            }}
+          >
             <img src="https://avatars.githubusercontent.com/u/9199763?v=4" alt="Profile pic" />
             <motion.span variants={anim.span} transition={{ type: "linear", duration: 0.05 }}>
               {section === "Contact" ? " Let's chat" : "Book a call"}
