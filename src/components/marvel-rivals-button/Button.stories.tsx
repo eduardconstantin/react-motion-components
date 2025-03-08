7a15c3bf2c22c16e54144028193e79df88e8a2f6
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2025-03-08 19:24:33 +0200

    Update stories

--- src/components/marvel-rivals-button/Button.stories.tsx
+++ src/components/marvel-rivals-button/Button.stories.tsx
@@ -1,22 +1,36 @@
 import type { StoryObj, Meta } from "@storybook/react";
-// import { ButtonType } from "./Button.d";
 import Button from "./Button";
 
 export default {
   title: "Components/Buttons/Marvel Rivals Button",
-  // args: {},
-  // argTypes: {},
+  args: {
+    text: "JOIN NOW",
+    buttonColor: 0,
+  },
+  argTypes: {
+    text: {
+      control: "text",
+      description: "Specify the text of your Button.",
+    },
+    buttonColor: {
+      control: { type: "range", min: 0, max: 360, step: 20 },
+      description: "The hue value to change the button color.",
+    },
+  },
   component: Button,
 } as Meta<typeof Button>;
 
 type Story = StoryObj<typeof Button>;
 
 export const Default: Story = {
-  // argTypes: {
-  // children: { control: false },
-  // randomBg: { control: false },
-  // buttonType: { control: false },
-  // buttonColor: { control: false },
-  // size: { control: false },
-  // },
+  argTypes: {
+    text: { control: false },
+    buttonColor: { control: false },
+  },
+};
+export const Playground: Story = {
+  args: {
+    text: "playground",
+    buttonColor: 180,
+  },
 };
